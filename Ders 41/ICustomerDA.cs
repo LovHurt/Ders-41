19777672c6769c5872aaaa5bee4344d862fb103e
Alican Dağıdır <alicandagidir@hotmail.com>
2023-10-26 15:48:17 +0300

    Add project files.

---
+++ Ders 41/ICustomerDA.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders_41
+{
+    internal interface ICustomerDA
+    {
+        void Add();
+        void Update();
+        void Delete();
+    }
+
+    class SQLServerCustomerDal:ICustomerDA
+    {
+        public void Add()
+        {
+            Console.WriteLine("Sql added");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Sql deleted");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Sql updated");
+        }
+    }
+    class OracleCustomerDal : ICustomerDA
+    {
+        public void Add()
+        {
+            Console.WriteLine("Oracle added");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Oracle updated");
+        }
+        public void Delete()
+        {
+            Console.WriteLine("Oracle deleted");
+        }
+    }
+    class MySqlCustomerDal : ICustomerDA
+    {
+        public void Add()
+        {
+            Console.WriteLine("MySql added");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("MySql updated");
+        }
+        public void Delete()
+        {
+            Console.WriteLine("MySql deleted");
+        }
+    }
+
+    class CustomerManager
+    {
+        public void Add(ICustomerDA customerDal)
+        {
+            customerDal.Add();
+        }
+    }
+}
---
+++ Ders 41/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders_41
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //InterfacesIntro1();
+
+            //Demo();
+
+            ICustomerDA[] customerDals = new ICustomerDA[3]
+            {
+                new SQLServerCustomerDal(),
+                new OracleCustomerDal(),
+                new MySqlCustomerDal()
+            };
+            foreach (var customerDal in customerDals)
+            {
+                customerDal.Add();
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void Demo()
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(new OracleCustomerDal());
+        }
+
+        private static void InterfacesIntro1()
+        {
+            PersonManager manager = new PersonManager();
+            Customer customer = new Customer
+            {
+                Id = 1,
+                FirstName = "Engin",
+                LastName = "Demiroğ",
+                Address = "Ankara"
+            };
+
+            Student student = new Student
+            {
+                Id = 1,
+                FirstName = "Derin",
+                LastName = "Demiroğ",
+                Departmant = "Cs"
+            };
+            manager.Add(student);
+            manager.Add(customer);
+        }
+    }
+    interface IPerson
+    {
+        int Id { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+
+    }
+    class Customer: IPerson
+    {
+        public int Id { get; set; } 
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+
+    }
+    class Student:IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Departmant { get; set; }
+    }
+    class Worker : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Departmant { get; set; }
+    }
+    class PersonManager
+    {
+        public void Add(IPerson person)
+        {
+            Console.WriteLine(person.FirstName);
+        }
+    }
+}
